6e14361bd426a67640545ce6edd001432aa6e2a1
pejhvaman <pejhvaman@gmail.com>
2025-06-28 18:30:38 +0330

    Done: Responsiveness and collapasible sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,15 +3,30 @@ import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "../components/AppNav";
 import Footer from "./Footer";
+import Button from "./Button";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function Sidebar() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleCollapse = () => {
+    setIsCollapsed((is) => !is);
+  };
+
   return (
-    <div className={styles.sidebar}>
+    <div
+      className={`${styles.sidebar} ${
+        isCollapsed ? styles.collapsedSidebar : ""
+      }`}
+    >
       <Logo />
       <AppNav />
       <Outlet />
       <Footer />
+      <Button type="collapse" onClick={handleCollapse}>
+        {isCollapsed ? "↓" : "↑"}
+      </Button>
     </div>
   );
 }
